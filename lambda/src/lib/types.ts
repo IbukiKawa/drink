00eae7c8bbb1e0c8396f2dd67ea1d944f0899d4b
IbukiKawa <jadsajd1234@gmail.com>
2026-02-06 17:39:44 +0900

    typesの定義

---
+++ lambda/src/lib/types.ts
@@ -0,0 +1,26 @@
+// DB用（DynamoDBの属性名に合わせる）
+export interface UserEntity {
+    Email: string;
+    Name: string;
+    Department: Department;
+    JoinYear: number;
+    Gender: Gender;
+    Floor: number;
+    CreatedAt: string;
+  }
+  
+  // API用（リクエストボディ）
+  export interface RegisterUserRequest {
+    email: string;
+    name: string;
+    department: Department;
+    joinYear: number;
+    gender: Gender;
+    floor: number;
+  }
+
+export const DEPARTMENT = ["金融", "製造", "エンタープライズ1", "エンタープライズ2", "エンタープライズ3", "Xイノベーション"] as const;
+export type Department = (typeof DEPARTMENT)[number];
+
+export const GENDER = ["男", "女"] as const;
+export type Gender = (typeof GENDER)[number];
