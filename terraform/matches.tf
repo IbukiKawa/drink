f85a5400b4ec42c1e2f3956ed57f4cb144135640
IbukiKawa <jadsajd1234@gmail.com>
2026-02-06 16:20:42 +0900

    matches.tfの追加

---
+++ terraform/matches.tf
@@ -0,0 +1,36 @@
+resource "aws_dynamodb_table" "matches_table" {
+  name         = "matches"
+  hash_key     = "Date"
+  range_key    = "TimeSlotMatchId"
+  billing_mode = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "Date"
+    type = "S"
+  }
+  attribute {
+    name = "TimeSlotMatchId"
+    type = "S"
+  }
+  attribute {
+    name = "User1Email"
+    type = "S"
+  }
+  attribute {
+    name = "User2Email"
+    type = "S"
+  }
+  global_secondary_index {
+    name            = "matchingEmailIndex"
+    hash_key        = "User1Email"
+    range_key       = "Date"
+    projection_type = "ALL"
+  }
+  global_secondary_index {
+    name            = "matchingEmailIndex"
+    hash_key        = "User2Email"
+    range_key       = "Date"
+    projection_type = "ALL"
+
+  }
+}
