e5eef8235d7ca3da85e31c5d3116c0b92cddab62
IbukiKawa <jadsajd1234@gmail.com>
2026-02-06 18:02:43 +0900

    フォーマットの整形

--- lambda/src/lib/dynamodb.ts
+++ lambda/src/lib/dynamodb.ts
@@ -1,6 +1,6 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
-const client = new DynamoDBClient({region: "ap-northeast-1"})
+const client = new DynamoDBClient({ region: "ap-northeast-1" });
 
 export const docClient = DynamoDBDocumentClient.from(client);
