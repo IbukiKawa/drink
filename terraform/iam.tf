c726914a746f42a226810a08ed422fa27acc57fe
IbukiKawa <jadsajd1234@gmail.com>
2026-02-11 09:04:08 +0900

    Iamロールの追加

---
+++ terraform/iam.tf
@@ -0,0 +1,48 @@
+resource "aws_iam_role" "lambda_role" {
+  name = "drink-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+
+}
+
+resource "aws_iam_role_policy" "lambda_dynamodb" {
+  name = "drink-lambda-dynamodb"
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:PutItem",
+          "dynamodb:GetItem",
+          "dynamodb:Query",
+          "dynamodb:UpdateItem"
+        ]
+        Resource = [
+          "aws_dynamodb_table.user_table.arn",
+          "aws_dynamodb_table.schedules_table.arn",
+          "aws_dynamodb_table.matches_table.arn",
+          "${aws_dynamodb_table.matches_table.arn}/index/*"
+        ]
+      }
+    ]
+  })
+}
